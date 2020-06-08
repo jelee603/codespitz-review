6a2c346fc8490e73c294f91b0cfef4afbcffb1d5
이지은 <jieun@MacBook-Air.local>
2020-06-08 23:20:01 +0900

    visitor 2

--- session75/3rd-10.js
+++ session75/3rd-10.js
@@ -1,5 +1,6 @@
 const Visitor = class {
     folder(task) { throw 'override'; }
+    parent(v, task) {throw 'override'; }
     task(v, task) { throw 'override'; }
 }
 
@@ -17,3 +18,31 @@ const ConsoleVisitor = class extends Visitor {
         return v + '-';
     }
 }
+
+const Renderer = class {
+    constructor (visitor) {this.visitor = visitor;}
+    render({task, list}) {
+        const v = this.visitor.folder(task);
+        this.subTask(this.visitor.parent(v, task), list);
+    }
+    subTask(parent, list) {
+        list.forEach(({task, list})=> {
+            cosnt v = this.visitor.task(parent, task);
+            this.subTask(this.visitor.parent(v, this), list);
+        });
+    }
+};
+
+
+const folder = new Task('s3-4');
+folder.add('2강교안작성');
+folder.add('3강교안작성');
+const {list} = folder.list('title');
+list[1].task.add('ppt정리');
+list[1].task.add('코드정리');
+const {list:sublist} = list[i].task.list('title');
+list[1].task.add('슬라이드마스터 정리');
+list[1].task.add('디자인개선');
+
+const renderer = new renderer(new DomVisitor('#p'));
+renderer.render(folder.list('title'));
